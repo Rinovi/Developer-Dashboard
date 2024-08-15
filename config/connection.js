b614b20dd4c4b819fe36cdc3afc33448206e7211
Caleb Warford <cjwarford98@gmail.com>
2024-08-14 20:53:24 -0700

    DB => DATABASE

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // initialize connection
-const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL)
     : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
-            host: 'localhost',
             dialect: 'postgres',
         }
     );
